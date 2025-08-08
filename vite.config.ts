4037ed121c62fb78839af1e856b4473b3893aeb9
Guilherme Miranda <guilherme.souza@grupoimg.com.br>
2025-08-07 21:20:09 -0300

    Set server host to '0.0.0.0' in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [vue(), vueJsx(), vueDevTools(), tailwindcss()],
   server: {
+    host: '0.0.0.0',
     port: Number(process.env.PORT) || 8080,
   },
   resolve: {
